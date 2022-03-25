534768f9e462ce95bad4428327e198a5ed9f149b
dengwenj <927561237@qq.com>
2022-03-25 18:50:10 +0800

    基本搭建

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: './src/index.ts',
+  output: {
+    filename: 'js/bunild.js',
+    path: path.resolve(__dirname, 'dist'),
+    pubilcPath: '/'
+  },
+  devServer: {
+    hot: 'only',
+    hot: true,
+    port: 1209
+  },
+  module: {
+
+  },
+  plugins: []
+}
