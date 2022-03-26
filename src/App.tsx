e054ecbd46cea40a83667e48f1961d41590ee9da
dengwenj <927561237@qq.com>
2022-03-26 16:29:34 +0800

    react基本配置完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
 
+import './App.css'
+import logo from './assets/logo192.png'
+
 export default function App() {
   return (
-    <div>Ap2p22111</div>
+    <div className='app'>
+      <img src={logo} alt="logo" />
+      <div className='wrapper'>
+        Welcome to the react app created by the CLI developed by Deng Wenjie
+      </div>
+      <div className='wrapper'>
+        If you want to know me this is my github:
+        <a 
+          className='info'
+          href="https://github.com/dengwenj"
+          target='_blank' 
+          rel="noreferrer"
+        >
+          dengwenj
+        </a>
+      </div>
+      <div className='wrapper'>
+        我是中国人🇨🇳，如果你也是，我叫邓文杰 (dèng wén jié). 你也可以在 
+        <a
+          className='info' 
+          href="https://weibo.com/u/6575004852" 
+          target='_blank' 
+          rel="noreferrer"
+        >
+          微博
+        </a>和
+        <a
+          className='info' 
+          href="https://www.zhihu.com/people/ni-shuo-bu-zou-de-98" 
+          target='_blank' 
+          rel='noreferrer'
+        >
+          知乎
+        </a>上找到我
+      </div>
+    </div>
   )
 }
