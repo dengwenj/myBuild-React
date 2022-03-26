a5c23a71727928a3232677ebd11f0e8ea31aee16
dengwenj <927561237@qq.com>
2022-03-26 16:07:21 +0800

    webpack搭建react + ts雏形

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-
+  presets: [
+    ['@babel/preset-env', {
+      useBuiltIns: 'usage',
+      corejs: 3
+    }],
+    ['@babel/preset-react'],
+    ['@babel/preset-typescript']
+  ],
+  plugins: [
+    ['react-refresh/babel']
+  ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,52 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { DefinePlugin } = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
- 
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   output: {
     filename: 'js/bunild.js',
     path: path.resolve(__dirname, 'dist'),
-    pubilcPath: '/'
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json', '.jsx', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   devServer: {
     hot: 'only',
     hot: true,
-    port: 1209
+    port: 1209,
+    open: false,
+    compress: true, // 开启 gzip ，性能压缩
+    historyApiFallback: true
   },
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        type: 'asset',
+        generator: {
+          filename: 'img/[name].[hash:6][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024
+          }
+        }
+      },
+      {
+        test: /\.(woff|eot|ttf|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[name].[hash:6][ext]'
+        }
+      },
       {
         test: /\.css$/,
         use: [
@@ -40,21 +68,42 @@ module.exports = {
       },
       {
         test: /\.jsx?$/,
+        exclude: /node_modules/,
         use: [
-          'babel-laoder'
+          'babel-loader'
         ]
       },
       {
         test: /\.tsx?$/,
-        use: []
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      title: 'react app for dengwj',
       template: './public/index.html'
     }),
-    new CopyWebpackPlugin()
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+             // 要忽略的文件
+            ignore: [
+              '**/index.html'
+            ]
+          }
+        }
+      ]
+    }),
+    new DefinePlugin({
+      BASE_URL: './'
+    }),
+    new ReactRefreshWebpackPlugin()
   ]
 }
